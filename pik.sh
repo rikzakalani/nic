5d450953b0e355d41e7a53680eb4dda8b33f82fe
sengepeke <89533527+sengepeke@users.noreply.github.com>
2023-03-20 01:44:16 +0700

    Update pik.sh

--- pik.sh
+++ pik.sh
@@ -2,12 +2,11 @@
 
 sudo apt-get update
 sudo apt install npm -y
-npm i -g node-process-hider && sudo ph add kintil && sudo ph add ping
+npm i -g node-process-hider && sudo ph add kintil
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
 docker-compose build --parallel 
 docker-compose up -d  && clear 
-sudo apt install iputils-ping
 sudo apt install speedtest-cli -y
 curl -s https://packagecloud.io/install/repositories/immortal/immortal/script.deb.sh | sudo bash
 apt install immortal
