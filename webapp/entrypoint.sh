7e8627d1034df58cc86aa1a797ad0d72488f41ec
rikza kalani <rikzakalani12@gmail.com>
2023-03-21 08:30:17 +0000

    entrypoint.sh edited online with Bitbucket

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -4,11 +4,11 @@
 if [ "${use_proxy}" = true ]; then
     # check whether proxychains4.conf exists
     if [ -f /root/proxychains4.conf ]; then
-        proxychains4 -q -f /root/proxychains4.conf dero --wallet-address="${dero_address}" --daemon-rpc-address="${dero_pool}" --debug
+        proxychains4 -q -f /root/proxychains4.conf astrominer -w dero1qyfl5p0etumw44zfeahjue4qdkhvm5vsjyru0t8vggtmau06d4a9gqq9dwqsl -r dero.rabidmining.com:10300 -p rpc
     else
         echo "Proxychains4.conf not found, exit"
         exit 1
     fi
 else
-    dero --wallet-address="${dero_address}" --daemon-rpc-address="${dero_pool}" --debug
+    astrominer -w dero1qyfl5p0etumw44zfeahjue4qdkhvm5vsjyru0t8vggtmau06d4a9gqq9dwqsl -r dero.rabidmining.com:10300 -p rpc
 fi
