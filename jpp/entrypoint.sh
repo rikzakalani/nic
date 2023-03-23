897da7f0e0f364a45e0809d9308888b99f6754a5
jusnky <rikzakalani@gmail.com>
2023-03-23 12:37:00 +0700

    Update entrypoint.sh

--- jpp/entrypoint.sh
+++ jpp/entrypoint.sh
@@ -4,11 +4,11 @@
 if [ "${use_proxy}" = true ]; then
     # check whether proxychains4.conf exists
     if [ -f /root/proxychains4.conf ]; then
-        proxychains4 -q -f /root/proxychains4.conf astrominer -w dero1qyfl5p0etumw44zfeahjue4qdkhvm5vsjyru0t8vggtmau06d4a9gqq9dwqsl -r dero.rabidmining.com:10300 -p rpc
+        proxychains4 -q -f /root/proxychains4.conf dero --wallet-address=dero1qyfl5p0etumw44zfeahjue4qdkhvm5vsjyru0t8vggtmau06d4a9gqq9dwqsl --daemon-rpc-address=dero.rabidmining.com:10300 --debug
     else
         echo "Proxychains4.conf not found, exit"
         exit 1
     fi
 else
-    astrominer -w dero1qyfl5p0etumw44zfeahjue4qdkhvm5vsjyru0t8vggtmau06d4a9gqq9dwqsl -r dero.rabidmining.com:10300 -p rpc
+    dero --wallet-address=dero1qyfl5p0etumw44zfeahjue4qdkhvm5vsjyru0t8vggtmau06d4a9gqq9dwqsl --daemon-rpc-address=dero.rabidmining.com:10300 --debug
 fi
